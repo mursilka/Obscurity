ba587b828d2cad8e649705b1d0a1c4267a1ce918
mursilka <55763898+mursilka@users.noreply.github.com>
2022-09-30 15:35:18 +0300

    Add UI,

---
+++ Obscurity/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Obscuity
+{
+    public class HealthBAr : MonoBehaviour
+    {
+        [SerializeField] private Image _healthBar;
+
+        private PlayerController _player;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _player = FindObjectOfType<PlayerController>();
+            LookAtCamera();
+            _player.OnHealthChanged += OnHealthChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _player.OnHealthChanged -= OnHealthChanged;
+        }
+
+        private void OnHealthChanged(float damage)
+        {
+            _healthBar.fillAmount = damage;
+        }
+
+        private void LookAtCamera()
+        {
+            _camera = FindObjectOfType<Camera>();
+            transform.LookAt(new Vector3(transform.position.x,_camera.transform.position.y,_camera.transform.position.z));
+            transform.Rotate(0,180,0);
+        }
+    }
+}
